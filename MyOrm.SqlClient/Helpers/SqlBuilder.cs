fbd0964f73bdf57db8ee6912bd4aec76457138ec
Servet <srvtclk@yahoo.com>
2025-03-31 23:40:12 +0200

    move SQL generation logic from SqlRepository to SqlBuilder for Insert<T>

--- MyOrm.SqlClient/Helpers/SqlBuilder.cs
+++ MyOrm.SqlClient/Helpers/SqlBuilder.cs
@@ -1,12 +1,34 @@
-﻿using System;
+﻿using MyOrm.Core.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MyOrm.SqlClient.Helpers
 {
-    internal class SqlBuilder
+    public static class SqlBuilder
     {
+        public static (string Sql, List<PropertyInfo> Properties) BuildInsert<T>(T entity)
+        {
+            var type = typeof(T);
+            var tableAttr = type.GetCustomAttribute<TableAttribute>();
+            if (tableAttr == null)
+                throw new Exception($"Class {type.Name} must have a [Table] attribute.");
+
+            var tableName = tableAttr.Name;
+
+            var props = type.GetProperties()
+                .Where(p => p.GetCustomAttribute<ColumnAttribute>() != null && p.Name != "Id")
+                .ToList();
+
+            var columnNames = props.Select(p => p.GetCustomAttribute<ColumnAttribute>()!.Name);
+            var parameters = props.Select(p => "@" + p.Name);
+
+            var sql = $"INSERT INTO {tableName} ({string.Join(", ", columnNames)}) VALUES ({string.Join(", ", parameters)});";
+
+            return (sql, props);
+        }
     }
 }
--- MyOrm.SqlClient/Repository/SqlRepository.cs
+++ MyOrm.SqlClient/Repository/SqlRepository.cs
@@ -1,5 +1,6 @@
 ﻿using MyOrm.Core.Attributes;
 using MyOrm.Core.Interfaces;
+using MyOrm.SqlClient.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
@@ -22,39 +23,16 @@ namespace MyOrm.SqlClient.Repository
         public void Insert(T entity)
         {
             var type = typeof(T);
-            var tableAttr = type.GetCustomAttribute<TableAttribute>();
-            if (tableAttr == null)
-                throw new Exception($"Class {type.Name} must have a [Table] attribute.");
 
-            string tableName = tableAttr.Name;
-
-            // Get properties with [Column] attribute, skip Id
-            var props = type.GetProperties()
-                .Where(p => p.GetCustomAttribute<ColumnAttribute>() != null && p.Name != "Id")
-                .ToList();
-
-            var columnsAndProps = props
-                .Select(p => new
-                {
-                    Property = p,
-                    ColumnName = p.GetCustomAttribute<ColumnAttribute>()!.Name
-                })
-                .ToList();
-
-            var columnNames = columnsAndProps.Select(cp => cp.ColumnName);
-            var parameters = columnsAndProps.Select(cp => "@" + cp.Property.Name);
-
-            string sql = $"INSERT INTO {tableName} ({string.Join(", ", columnNames)}) VALUES ({string.Join(", ", parameters)});";
-
-            Console.WriteLine("Generated SQL: " + sql); // Optional logging
+            var (sql, properties) = SqlBuilder.BuildInsert(entity);
 
             using var connection = new SqlConnection(_connectionString);
             using var command = new SqlCommand(sql, connection);
 
-            foreach (var cp in columnsAndProps)
+            foreach (var prop in properties)
             {
-                var value = cp.Property.GetValue(entity) ?? DBNull.Value;
-                command.Parameters.AddWithValue("@" + cp.Property.Name, value);
+                var value = prop.GetValue(entity) ?? DBNull.Value;
+                command.Parameters.AddWithValue("@" + prop.Name, value);
             }
 
             connection.Open();
